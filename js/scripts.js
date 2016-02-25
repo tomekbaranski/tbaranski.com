f6d6c9ff076e04bd6cac9d96e5321b37bb3a5033
tomek_baranski <tomek_baranski@wp.pl>
2016-02-25 23:10:50 +0100

    Dodanie zawartości strony

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+	$("ul#nav li:nth-child(1)").css("margin-right", "60px");
+	$("ul#nav li:nth-child(2)").css("margin-right", "316px");
+	$("ul#nav li:nth-child(3)").css("margin-right", "60px");
+	$("ul#nav li:nth-child(4)").css("margin-right", "0px");
+});
